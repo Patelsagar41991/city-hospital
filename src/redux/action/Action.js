e87e581b46b947e1231035d3759e5cedc8744033
sagar41991 <sagar41991>
2022-08-25 08:01:25 +0530

    24-8-22

--- src/redux/action/Action.js
+++ src/redux/action/Action.js
@@ -1,10 +1,8 @@
 import * as Actiontypes from '../ActionType'
 
 export const signinAction = (values) => (dispatch) => {
-
     dispatch({ type : Actiontypes.SIGN_UP, payload : values})
 }
 export const signUpAction = (values) => (dispatch) => {
-
     dispatch({ type : Actiontypes.SIGN_IN, payload : values})
 }
